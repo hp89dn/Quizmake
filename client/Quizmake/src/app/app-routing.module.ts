90c0abc8133215e6f536c0944c87f1ba7b34edc2
Huynh Van Phuoc <huynhphvan@gmail.com>
2020-07-13 16:00:21 +0700

    create pages module and component

--- client/Quizmake/src/app/app-routing.module.ts
+++ client/Quizmake/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
+import { PagesComponent } from './pages/pages.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pages',
+    component: PagesComponent
+  },
+  {
+    path: '',
+    redirectTo: 'pages',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'pages',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
