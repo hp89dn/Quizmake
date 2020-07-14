16b82954aa84a0fde9c7ad755c1382be59fa6e48
Huynh Van Phuoc <huynhphvan@gmail.com>
2020-07-14 16:42:27 +0700

    update docx-reader

--- server/controllers/tools/docx-reader.js
+++ server/controllers/tools/docx-reader.js
@@ -1,39 +1,49 @@
 exports.docxReader = (req, res, next) => {
-  var textract = require("textract");
+  const textract = require("textract");
 
-  const docxUrl = req.body.docxUrl;
-  var config = {
+  const docxUrl = req.body.docxUrl; // url of file docx
+
+  // config for textract module
+  const config = {
     preserveLineBreaks: true,
     // preserveOnlyMultipleLineBreaks: true
   };
-  // https://firebasestorage.googleapis.com/v0/b/programing-quiz-2019.appspot.com/o/input.docx?alt=media&token=21bea245-f9d4-47b9-908a-92b2a8054cd3
+
+  // start to extract text in docx to text
   textract.fromUrl(docxUrl, config, function (error, text) {
-    if (error) {
+    if (error) { // Cannot read file docx
       res.status(500).json({
         message: "Error to read file docx.",
       });
     } else {
-      const text_list = text.split("\n\n").filter((m) => m !== "");
-      const quiz_list = [];
-      text_list.forEach((q) => {
-        const quiz_split = q.split("\n");
-        const len = quiz_split.length;
 
+      const text_list = text.split("\n\n").filter((m) => m !== ""); // split all readed text to quiz texts and remove some of empty quiz
+      const quiz_list = []; // array of quiz to return json to client
+      
+      text_list.forEach((q) => {
+        const quiz_split = q.split("\n"); // split lines of text to elements of array
+        const len = quiz_split.length; // length of array of elements splited text
+        const question = quiz_split.slice(1, len - 4).join('\n'); // ['Trả lời câu hỏi sau:', '1+1=?'] => "Trả lời câu hỏi sau:\n1+1=?""
+        
         // list of answers
-        const answer_list = quiz_split.slice(len - 4, len);
+        const answer_list = quiz_split.slice(len - 4, len); // get list answers
         const correct = String(
           answer_list.find((ans) => ans.includes("*"))
-        ).replace("*", "");
-        const answer = answer_list.map((a) => a.replace("*", ""));
-
+        ).replace("*", ""); // find correct answer and remove '*' in string
+        const replaced_answer = answer_list.map((a) => a.replace("*", "")); // remove '*' in list of answers
+        
+        // create quiz object
         const quiz = {
           point: quiz_split[0],
-          question: quiz_split.slice(1, len - 4),
-          answer: answer,
+          question: question,
+          answer: replaced_answer,
           correct: correct,
         };
+        // push quiz to quiz-list
         quiz_list.push(quiz);
       });
+
+      // return json to client
       res.status(200).json(
         quiz_list
       );
