efdf7704fbfb2ae77164be3748097603757e24d0
Huynh Van Phuoc <huynhphvan@gmail.com>
2020-07-16 22:12:38 +0700

    update giao dien

--- client/Quizmake/src/app/pages/lecturer-page/create-quiz/create-quiz-routing.module.ts
+++ client/Quizmake/src/app/pages/lecturer-page/create-quiz/create-quiz-routing.module.ts
@@ -1,12 +1,29 @@
-import { CreateQuizComponent } from './../create-quiz/create-quiz.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// components
+import { QuizManagementComponent } from './quiz-management/quiz-management.component';
+import { CreateQuizComponent } from './../create-quiz/create-quiz.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: CreateQuizComponent
+    component: CreateQuizComponent,
+    children: [
+      {
+        path: 'quiz-management',
+        component: QuizManagementComponent
+      },
+      {
+        path: '',
+        redirectTo: 'quiz-management',
+        pathMatch: 'full'
+      },
+      {
+        path: '**',
+        redirectTo: 'quiz-management'
+      }
+    ]
   }
 ];
 
--- client/Quizmake/src/app/pages/lecturer-page/create-quiz/create-quiz.component.ts
+++ client/Quizmake/src/app/pages/lecturer-page/create-quiz/create-quiz.component.ts
@@ -1,103 +1,17 @@
-import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } from "@angular/core";
-import {
-  CdkDragDrop,
-  moveItemInArray,
-  transferArrayItem,
-} from "@angular/cdk/drag-drop";
-
-// service
-import { DocxReaderService } from './../services/docx-reader.service';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
 
 @Component({
   selector: "app-create-quiz",
   templateUrl: "./create-quiz.component.html",
-  styleUrls: ["./create-quiz.component.scss"],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ["./create-quiz.component.scss"]
 
 })
 export class CreateQuizComponent implements OnInit {
-  constructor(private DocxReaderService: DocxReaderService,private cdr: ChangeDetectorRef,) {}
 
+  @Input() quizData: any;
+
+  constructor() {}
   
-  operationListSubcribe: Subscription;
-
-  point: number;
-  public question: string;
-  public answerList: any[] = [
-    '', '', '', ''
-  ];
-
-  quizList: any = [
-    {
-      point: "(1 pount)",
-      question: "What is the scientific name of a butterfly?",
-      answers: ["Apis", "Coleoptera", "Formicidae", "Rhopalocera"],
-      correct: "B. 2",
-    },
-    // {
-    //   point: "(1 pount)",
-    //   question: "How hot is the surface of the sun?",
-    //   answers: ["1,233 K", "5,778 K", "12,130 K", "101,300 K"],
-    //   correct: "B. 2",
-    // },
-    // {
-    //   point: "(1 pount)",
-    //   question: "Who are the actors in The Internship?",
-    //   answers: [
-    //     "Ben Stiller, Jonah Hill",
-    //     "Courteney Cox, Matt LeBlanc",
-    //     "Kaley Cuoco, Jim Parsons",
-    //     "Vince Vaughn, Owen Wilson",
-    //   ],
-    //   correct: "B. 2",
-    // },
-    // {
-    //   point: "(1 pount)",
-    //   question: "What is the capital of Spain?",
-    //   answers: ["Berlin", "Buenos Aires", "Madrid", "San Juan"],
-    //   correct: "C. 3",
-    // },
-  ];
-
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.quizList, event.previousIndex, event.currentIndex);
-  }
-
-  addAnswerBox() {
-    this.answerList.push("");
-  }
-  removeAnswerBox(index) {
-    this.answerList.splice(index, 1);
-  }
-  removeQuizBox(index) {
-    this.quizList.splice(index, 1);
-  }
-
-  // uploader
-  isHovering: boolean;
-
-  files: File[] = [];
-
-  toggleHover(event: boolean) {
-    this.isHovering = event;
-  }
-
-  onDrop(files: FileList) {
-    for (let i = 0; i < files.length; i++) {
-      this.files.push(files.item(i));
-    }
-  }
-
-  getQuizData($event) {
-    this.DocxReaderService.create({
-      docxUrl: $event
-    }).subscribe(( res : any) => {
-      console.log(res);
-      this.quizList = [...res];
-    });
-  }
-
   ngOnInit() {
     
   }
